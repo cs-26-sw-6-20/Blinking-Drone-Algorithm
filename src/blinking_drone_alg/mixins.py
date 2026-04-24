8d7d21ef95bd059e004cf422df299eb473c7f94f
Kresten Laust <krestenlaust@gmail.com>
2026-04-24 13:51:58 +0200

    Add _as factory mixin method

--- src/blinking_drone_alg/mixins.py
+++ src/blinking_drone_alg/mixins.py
@@ -1,5 +1,21 @@
-from dataclasses import dataclass, replace
+from dataclasses import dataclass, replace, fields
+
 
 class WithMixin:
     def with_(self, **kwargs):
+        """
+        Mutates specific fields of a copy of the object.
+        :param kwargs: The field changes of the object.
+        :return: The new object with overridden fields.
+        """
         return replace(self, **kwargs)
+
+    def as_(self, cls, **kwargs):
+        """
+        Mutates specific fields of a copy of the object, into a new class with similar fields.
+        :param cls: The target class
+        :param kwargs: The fields changes of the target class
+        :return: The new object with overridden fields.
+        """
+        existing = {f.name: getattr(self, f.name) for f in fields(self)}
+        return cls(**{**existing, **kwargs})
